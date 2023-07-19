ec973ca37512acc2cb385d08ad615e33e39a3792
Makiusz <34923136+Makiusz@users.noreply.github.com>
2023-07-19 21:25:47 +0200

    Add files via upload

--- zadanie3.php
+++ zadanie3.php
@@ -2,23 +2,51 @@
 
 class RankingTable{
 
-    public array $playerList;
+    private $playerList = array();
 
-    function __construct($playerList){
-        $this->playerList = $playerList;
+    function __construct($playerName){
+        foreach($playerName as $name){
+            $this->playerList[$name] = array(
+                'score' => 0,
+                'gamesPlayed' => 0
+            );
+        }
+        
     }
 
     public function recordResult($name,$score){
-       ## TO DO!
+        $this->playerList[$name]['score']+=$score;
+        $this->playerList[$name]['gamesPlayed']++;
     }
-
+    private function sortPlayers(){
+        uasort($this->playerList, function ($a, $b){
+            if($a['score'] == $b['score']){
+                if($a['gamesPlayed'] == $b['gamesPlayed']){
+                    return 0;
+                }
+                return ($a['gamesPlayed']>$b['gamesPlayed']) ? 1 : -1;
+            }
+            return ($a['score']>$b['score']) ? -1 : 1;
+        });
+    }
+    
     public function playerRank($number){
-        return $this->playerList[$number-1];
-    }
+        $this->sortPlayers();
+        $sortedPlayers = array_keys($this->playerList);
+        return $sortedPlayers[$number - 1];
+    } 
+  
 }
 
 
 
 ## TEST ##
 $table = new RankingTable(array('Jan', 'Maks', 'Monika'));
-echo $table->playerRank(1);
+$table->recordResult('Jan', 2);
+$table->recordResult('Maks', 3);
+$table->recordResult('Maks', 2);
+$table->recordResult('Monika', 5);
+
+echo $table->playerRank(1), "</br>";
+echo $table->playerRank(2), "</br>";
+echo $table->playerRank(3), "</br>";
